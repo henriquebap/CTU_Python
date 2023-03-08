057fada5040496ddeb1638b74fad2e64a24c8c0b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-08 11:55:32 -0300

    ajustes no if

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -20,22 +20,21 @@ IMC = peso/altura** 2
 print(f"Seu IMC é {IMC}")
 
 if masculino: 
-    if (IMC <= 16):
+    if (IMC <= 16 and IMC < 18):
         print(f"{name}, Voce está com 'Magreza'")
-    elif(IMC <= 19):
+    elif(IMC > 18 and IMC < 29.9):
         print(f"{name}, CUIDADO! Voce esta com 'Eutrofia'; seu IMC está normal")
-    elif(IMC <= 30.9):
+    elif(IMC > 30.9 and IMC < 34.4):
         print(f"{name}, Voce está muito gordo (Sobre peso)")
     else:
         print(f"{name}, Voce esta com Obesidade")
 
 if(feminine):
-    if (IMC <= 18):
+    if (IMC <= 18 and IMC < 18.1):
         print(f"{name}, Voce está com 'Magreza'")
-    elif(IMC <= 18,2):
+    elif(IMC <= 18.2 and IMC < 33.7):
         print(f"{name}, CUIDADO! Voce esta com 'Eutrofia'; seu IMC está normal")
-    elif(IMC <= 33.8):
+    elif(IMC <= 33.8 and IMC < 36):
         print(f"{name}, Voce está muito gordo (Sobre peso)")
     else:
         print(f"{name}, Voce esta com Obesidade")
-        
