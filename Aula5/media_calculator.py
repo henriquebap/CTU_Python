258cc7e3931f5e977f2bf51099bcf15637b26085
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-15 11:57:38 -0300

    aula 5 sobre def, atributo, etc

---
+++ Aula5/media_calculator.py
@@ -0,0 +1,30 @@
+###3 checkpoints 2 valem 20%
+# Global sc (prova) 30% 
+# Challenge 50%
+# a soma de tudo vale 40% no bimestre
+print("##########################################")
+print("######## CALCULADORA MEDIA FIAP ##########")
+print("###########################################")
+
+def calculate_rating(first_checkp, sec_checkp, thir_checkpoint, global_sc, challenge):
+    media_cp = 0
+    if (first_checkp < sec_checkp and sec_checkp < thir_checkpoint):
+        media_cp = (sec_checkp + thir_checkpoint)/ 2
+    elif (first_checkp > sec_checkp and sec_checkp < thir_checkpoint ):
+        media_cp = (first_checkp + thir_checkpoint) / 2
+    else:
+        media_cp = (first_checkp + sec_checkp) / 2
+
+    media_total = (media_cp * 0.20) + (global_sc * 0.30) + (challenge * 0.50)
+    return media_total
+
+first_checkp = float(input("Digite sua primeira nota do Checkpoint: "))
+sec_checkp = float(input("Digite a nota do seu Segundo Checkpoint: "))
+thir_checkpoint = float(input("Digite a terceira nota do seu Checkpoint: "))
+global_sc_nota = float(input("Sua nota do global Solution: "))
+challenge = float(input("Sua nota no Challenge: "))
+
+media = calculate_rating(first_checkp, sec_checkp, thir_checkpoint, global_sc_nota, challenge)
+
+print(f"sua media do checkpoint é: {media}")
+
