906e3fe045f2b23e820767af094608b04435f907
Henriquebap <henriquebaptista2003@gmail.com>
2023-03-24 16:56:43 -0300

    Arrumando os arquivos, dicionario dev, teste de codigo e aplicacao de dicionario em codigo

---
+++ dicionario_24-03-23/dicionario_ex.py
@@ -0,0 +1,27 @@
+user_data = {
+    'first_name': 'Leonardo',
+    'last_name': 'Bragatti',
+    'age': 18
+}
+last_name = user_data['last_name']
+
+print(user_data['last_name'])
+
+#para adicionar no dicionario(manipulacao)
+dic = {}
+dic['name'] = 'Henrique'
+print(dic)
+dic['last_name'] = 'Baptista'
+print(dic)
+
+#another way to manipulate the dict
+my_dict = {}
+my_dict.update({
+    'First_name': 'Henrique',
+    'Last_name': 'Baptista',
+    'age': 20
+})
+print(my_dict)
+
+
+####
---
+++ dicionario_24-03-23/testedicionario.py
@@ -0,0 +1,50 @@
+def display_user_data(user):
+    print("Primeiro nome: " + user['first_name'])
+    print("Último Nome: " + user['last_name'])
+    print("Idade: " + str(user['age']))
+    print()
+
+
+def create_user(users):
+    first_name = input("Qual seu primeiro nome: ")
+    last_name = input("Qual seu último nome: ")
+    age = int(input("Qual sua idade: "))
+    user_data = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'age': age,
+    }
+    users.append(user_data)
+
+
+def search_user(users):
+    first_name = input("Qual usuário deseja visualizar os dados: ")
+    for user in users:
+        if user['first_name'] == first_name:
+            display_user_data(user)
+            return
+
+    print("Usuário não encontrado")
+    print()
+
+def show_users(users):
+    for user in users:
+        display_user_data(user)
+
+users = []
+while True:
+    print("Selecione a opção desejada")
+    print("1 - Cadastrar usuário")
+    print("2 - Localizar usuário pelo primeiro nome")
+    print("3 - Ver todos os usuários")
+    print("0 - Sair")
+    option = int(input())
+    if option == 0:
+        break
+    
+    if option == 1:
+        create_user(users)
+    elif option == 2:
+        search_user(users)
+    elif option == 3:
+        show_users(users)
