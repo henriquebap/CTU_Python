f3e18e37ca600340ec3b0d553b0ef7bb5dc38e44
henriquebap <henriquebaptista2003@gmail.com>
2023-04-18 19:33:32 -0300

    arrumando a reclamacao de codigo da maquina depois do pull

--- revisao_31_03/aula.py
+++ revisao_31_03/aula.py
@@ -2,12 +2,13 @@ def select_option():
     print("1 - Cadastrar o Aluno")
     print("2 - Lancar notas")
     print("3 - Consultar aluno")
+    print("4 - Listar todos os estudantes")
     print("0 - Sair")
 
     option = int(input())
     return option
 
-def creat_student():
+def create_student():
     first_name = input("Digite o seu nome: ")
     last_name = input("Digite o seu sobre nome: ")
     student_class = input("Digite a sua turma: ")
@@ -20,13 +21,27 @@ def creat_student():
     }
     return student
 
+def get_student(students):
+    """
+    A função localiza e retorna um estudante
+    Tem como parâmetro a lista de estudantes onde
+    será realizada a busca
+    Retorna um dicionário com os dados do estudante
+    caso seja localizado.
+    Se não achar o estudante, não retorna nada
+    """
+    identification = input("Digite o RM do aluno: ")
+    for student in students:
+        # Verifica atráves da chave id se o conteúdo
+        # da chave é igual ao dado informado pelo usuário
+        if student['id'] == identification:
+            return student
+
 def show_students(students):
     identification = input("Digite o Rm do Aluno: ")
     for student in students:
         if student['id'] == identification:
-            print(student)
-            return
-    print("Nao foi encontrado esse aluno!")
+            return student
 
 def insert_grades(students):
     student = get_student(students)
@@ -46,15 +61,29 @@ def insert_grades(students):
     student['global_solution'] = global_solution
     student['checkpoints'] = checkpoints
 
+def show_students(students):
+    for student in students:
+        print(student)
+
 students = []
-option = select_option()
-while option != 0:
+while True:
+    option = select_option()
+    if option == 0:
+        break
     if option == 1:
-        student = creat_student()
+        student = create_student()
         students.append(student)
     elif option == 2:
         insert_grades(students)
     elif option == 3:
         show_students(students)
-    option = select_option()
-print(students)
+        if student:
+            print(student)
+        else:
+            print("Aluno nao foi encontrado!")
+            confirmation = input("Deseja cadastrar um novo aluno? (s/n)")
+            if confirmation == 's':
+                student = create_student()
+                students.append(student)
+    elif option == 4:
+        show_students(students)
