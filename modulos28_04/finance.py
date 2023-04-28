e194103b3f05a5912d3ff50fbf076fd0dd6a6c38
henriquebap <henriquebaptista2003@gmail.com>
2023-04-28 11:50:45 -0300

    Aprendendo a usar modulos/Import/from/as etc..

---
+++ modulos28_04/finance.py
@@ -0,0 +1,5 @@
+tax = 0.10
+
+def calculate_value(value):
+    total = value *(1 + tax)
+    return total
---
+++ modulos28_04/main.py
@@ -0,0 +1,5 @@
+import finance
+
+value = finance.calculate_value(100)
+print(f"O valor final e {value}")
+
