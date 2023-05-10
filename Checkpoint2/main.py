1d5d82e189649c80c5ed9253535439c5b8989a46
henriquebap <henriquebaptista2003@gmail.com>
2023-05-09 22:34:30 -0300

    Finalizando os tratamentos sem nenhum erro de entrada ou quebra de codigo

--- Checkpoint2/main.py
+++ Checkpoint2/main.py
@@ -8,8 +8,14 @@ def option():
     print("0 - Sair")
     print("----" * 10)
 
-    Option = int(input())
-    return Option
+    while True:
+        Option = input()
+        if Option.isdigit():
+            Option = int(Option)
+            return Option
+        else:
+            print("Digite uma opcao valida")
+            return Option
 
 def save_store(store_id, store_name):
     store_disc = {
@@ -36,23 +42,23 @@ stores = []
 while True:
     Option = option()
     if Option == 1:
-       store = store_cad()
-       if store:
-           save_store(store['store_id'], store['store_name'])
-           print("Loja cadastrada com sucesso\n")
-       else:
-           print("Erro: dados de loja invalidos")
+        store = store_cad()
+        if store:
+            save_store(store['store_id'], store['store_name'])
+            print("Loja cadastrada com sucesso\n")
+        else:
+            print("Erro: dados de loja invalidos")
     elif Option == 2:
         if len (stores) == 0:
-                print("Nenhuma Loja Cdastrada")
+             print("Nenhuma Loja Cdastrada")
         else:
             store_id = input("Digite o ID da loja: ")
             if store_id == store['store_id']:
-                    prod = cad_prod()
-                    if save_product(store_id,prod['prod_id'], prod['prod_nm'], prod['prod_desc'], prod['prod_price']):
-                        print("Produto cadastrado com sucesso\n")
-                    else:
-                        print("Produto nao cadastrado")
+                prod = cad_prod()
+                if save_product(store_id,prod['prod_id'], prod['prod_nm'], prod['prod_desc'], prod['prod_price']):
+                    print("Produto cadastrado com sucesso\n")
+                else:
+                    print("Produto nao cadastrado")
             else:
                 print("Loja nao encontrada")
     elif Option == 3:
@@ -69,6 +75,4 @@ while True:
                     print("Preco:", product['prod_price'],"\n")
     elif Option == 0:
         break
-    else:
-        print("Digite uma opcao valida")
-
+    
