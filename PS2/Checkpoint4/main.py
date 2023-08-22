393a002d34b83aef5b0a3dd7887d21fdc3f8e493
Henriquebap <henriquebaptista2003@gmail.com>
2023-08-21 21:43:25 -0300

    Grande avanco do projeto, Criando e instanciando varias funcoes, organizando o codigo com Opcao em cada funcao e adicionando funcionalidade extras

--- PS2/Checkpoint4/main.py
+++ PS2/Checkpoint4/main.py
@@ -1,6 +1,6 @@
 import csv
-from tools import get_file, title_rate_search, organizing, treshold, filter_title_rate, org_rate_search
-from tools import avarege_comparing
+from tools import get_file, title_rate_search, organizing, treshold, filter_title_rate, org_rate_search,option_rate
+from tools import avarege_comparing, budget_search, calculate_budget_difference,gross_search, option, budget_calculator_without_avengers,option_budget,option_gross,gross_search_w_avengers
 
 def main():
     file_path = "H:\\User\\Desktop\\rqiue2.0\\Fiap\\CT_Python_Class\\CTU_Python\\PS2\\Checkpoint4\\db.csv"
@@ -8,6 +8,102 @@ def main():
     if not file_content: 
         return
 
+
+    while True:
+        Option = option()
+        if Option == 1:
+            print("---"*3)
+
+            org_avg_ratings = org_rate_search(file_content)
+
+            print("Média de Notas divido por Empresa:")
+            for org, avg_rating in org_avg_ratings.items():
+                print(f"Empresa: {org}, Com uma Média de nota de: {avg_rating:.2f}")
+    
+            avarege_comparing(file_content)
+
+        elif Option == 2:  
+                while True:
+                    option_bud = option_budget()
+                    if option_bud == 1:   
+                        highest_budget_company, highest_budget, lowest_budget_company, lowest_budget = budget_search(file_content)
+                        print("-----"*10)
+                        print(f"A Empresa com o maior orçamento é a  {highest_budget_company} com um orçamento total de ${highest_budget:.2f}")
+                        print(f"e a {lowest_budget_company} e a menor empresa com o orçamento de ${lowest_budget:.2f}")
+                    elif option_bud == 2:
+                        budget_difference = calculate_budget_difference(highest_budget, lowest_budget)
+                        print("-----"*10)
+                        print(f"A diferença do orçamento entre as empreas é de ${budget_difference:.2f}")
+                    elif option_bud == 3:        
+                        highest_budget_company, highest_budget, lowest_budget_company, lowest_budget = budget_calculator_without_avengers(file_content)
+                        print("-----"*10)
+                        print("Tirando Avengers da Lista o resultado fica....")
+                        print(f"A Empresa com o maior orçamento mesmo sem avengers é {highest_budget_company} com um orçamento total de ${highest_budget:.2f}")
+                        print(f"Infelizmente a {lowest_budget_company} continua sendo a menor empresa com o orçamento de ${lowest_budget:.2f}")
+                    elif option_bud == 0:
+                        break
+                    else:
+                        print("Porfavor Digite uma opcao valida")
+        elif Option == 3:
+            while True:
+                    Option_gross = option_gross()
+                    if Option_gross == 1:        
+                        print("-----"*10)
+                        gross_earnings = gross_search(file_content)
+                        highest_gross_company = max(gross_earnings, key=gross_earnings.get)
+                        highest_gross_earnings = gross_earnings[highest_gross_company]
+                        print(f"A Empresa com um maior faturamento pelo mundo todo é {highest_gross_company} Com um total de Ganhos de ${highest_gross_earnings:.2f}")
+                    elif Option_gross == 2:
+                        print("-----"*10)
+                        lowest_gross_company = min(gross_earnings, key=gross_earnings.get)
+                        lowest_gross_earnings = gross_earnings[lowest_gross_company]
+                        print(f"A empresa {lowest_gross_company} teve um faturamento de ${lowest_gross_earnings}")
+                    elif Option_gross == 3:
+                        print("-----"*10)
+                        lowest_gross_company = min(gross_earnings, key=gross_earnings.get)
+                        lowest_gross_earnings = gross_earnings[lowest_gross_company]
+                        earnings_difference = highest_gross_earnings - lowest_gross_earnings
+                        print(f"A diferença de faturamento entre as duas é de ${earnings_difference:.2f}")
+                    elif Option_gross == 4:
+                        print("-----"*10)
+                        gross_earnings = gross_search_w_avengers(file_content)
+                        highest_gross_company = max(gross_earnings, key=gross_earnings.get)
+                        highest_gross_earnings = gross_earnings[highest_gross_company]
+                        print("*Removendo Avengers*")
+                        print(f"A Empresa com um maior faturamento pelo mundo todo é {highest_gross_company} Com um total de Ganhos de ${highest_gross_earnings:.2f}")
+                    elif Option_gross == 0:
+                        break
+                    else:
+                        print("Digite uma Opcao Valida!")
+        elif Option == 4:
+            while True:
+                Option_rate = option_rate()
+                if Option_rate == 1:
+                    filter_grade = input("Coloque uma nota para filtrar os filmes: ")
+                    filter_grade = treshold(filter_grade)
+                    if filter_grade > 10:
+                        print("Porfavor Digite um numero entre 0 e 10")
+                    else:
+                        continue
+                    title_rate = title_rate_search(file_content)
+                    organizing(title_rate)
+                    
+                    filtered_titles_and_ratings = filter_title_rate(filter_grade, title_rate)
+
+                    print("---"*3)
+                    print(f"O(s) Filme(s) com a média igual ou maior de {filter_grade}, em Ordem do Melhor para o Pior:")
+                    print("---"*3)
+                    for title_rating in filtered_titles_and_ratings:
+                        print(f"Titulo: {title_rating['Title']}, Nota: {title_rating['Rating']}")
+                elif Option_rate == 0:
+                    break
+                else:
+                    print("Digite uma opcao valida!")
+
+
+
+
+'''
     filter_grade = input("Coloque a nota que queira ver: ")
     filter_grade = treshold(filter_grade)
 
@@ -22,15 +118,40 @@ def main():
     for title_rating in filtered_titles_and_ratings:
         print(f"Title: {title_rating['Title']}, Rating: {title_rating['Rating']}")
         
-    print("---"*3)
-
-    org_avg_ratings = org_rate_search(file_content)
-
-    print("Average Ratings by Company:")
-    for org, avg_rating in org_avg_ratings.items():
-        print(f"Company: {org}, Average Rating: {avg_rating:.2f}")
+    #print("---"*3)
+#
+ #   org_avg_ratings = org_rate_search(file_content)
+#
+ #   print("Average Ratings by Company:")
+  #  for org, avg_rating in org_avg_ratings.items():
+   #     print(f"Company: {org}, Average Rating: {avg_rating:.2f}")
     
-    avarege_comparing(file_content)
+    #avarege_comparing(file_content)
+
+    
+    highest_budget_company, highest_budget, lowest_budget_company, lowest_budget = budget_search(file_content)
+    print(f"The company with the highest total budget is {highest_budget_company} with a total budget of ${highest_budget:.2f}")
+    print(f"e a {lowest_budget_company} e a menor empresa com lucro de ${lowest_budget:.2f}")
+
+
+    budget_difference = calculate_budget_difference(highest_budget, lowest_budget)
+
+    print(f"The difference between the highest and lowest budgets is ${budget_difference:.2f}")
+
+    gross_earnings = gross_search(file_content)
+    highest_gross_company = max(gross_earnings, key=gross_earnings.get)
+    highest_gross_earnings = gross_earnings[highest_gross_company]
+
+    print(f"The company with the highest gross worldwide earnings is {highest_gross_company} with a total earnings of ${highest_gross_earnings:.2f}")
+    
+    lowest_gross_company = min(gross_earnings, key=gross_earnings.get)
+    lowest_gross_earnings = gross_earnings[lowest_gross_company]
+
+    earnings_difference = highest_gross_earnings - lowest_gross_earnings
+    print(f"The difference between the highest and lowest gross earnings is ${earnings_difference:.2f}")
+    
+'''
 
 if __name__ == "__main__":
     main()
+    
--- PS2/Checkpoint4/tools.py
+++ PS2/Checkpoint4/tools.py
@@ -31,6 +31,10 @@ def filter_title_rate(result, title_rate):
             for title_rating in title_rate
             if title_rating["Rating"] >= result
 ]
+    
+    if not filtered_titles_and_ratings:
+        print("Nao Existe nenhum filme com essa nota.")
+
     return filtered_titles_and_ratings
 
 def org_rate_search(reader):
@@ -87,4 +91,140 @@ def budget_search(reader):
     highest_budget_company = max(bud_totals, key=bud_totals.get)
     highest_budget = bud_totals[highest_budget_company]
     
-    return highest_budget_company, highest_budget
+    lowest_budget_company = min(bud_totals, key=bud_totals.get)
+    lowest_budget = bud_totals[lowest_budget_company]
+    
+    return highest_budget_company, highest_budget, lowest_budget_company, lowest_budget
+
+
+def budget_calculator_without_avengers(reader):
+    bud_totals = {}
+    
+    for row in reader:
+        budget = float(row["Budget"])
+        org = row["Company"]
+        title = row["Original Title"]
+
+        # Exclude movies with "Avengers" in the title
+        if "Avengers" not in title:
+            # Update the company's total budget
+            if org in bud_totals:
+                bud_totals[org] += budget
+            else:
+                bud_totals[org] = budget
+    
+    # Find the company with the highest total budget
+    highest_budget_company = max(bud_totals, key=bud_totals.get)
+    highest_budget = bud_totals[highest_budget_company]
+    
+    lowest_budget_company = min(bud_totals, key=bud_totals.get)
+    lowest_budget = bud_totals[lowest_budget_company]
+    
+    return highest_budget_company, highest_budget, lowest_budget_company, lowest_budget
+
+def calculate_budget_difference(highest_budget, lowest_budget):
+    difference = highest_budget - lowest_budget
+    return difference
+
+
+def gross_search(reader):
+
+    gross_totals = {}
+    
+    for row in reader:
+        gross_earnings = float(row["Gross Worldwide"])
+        org = row["Company"]
+
+        # Update the company's total gross earnings
+        if org in gross_totals:
+            gross_totals[org] += gross_earnings
+        else:
+            gross_totals[org] = gross_earnings
+    
+    return gross_totals
+
+
+def gross_search_w_avengers (reader):
+
+    gross_totals = {}
+    
+    for row in reader:
+        gross_earnings = float(row["Gross Worldwide"])
+        org = row["Company"]
+        title = row["Original Title"]
+
+        if "Avengers" not in title:
+            if org in gross_totals:
+                gross_totals[org] += gross_earnings
+            else:
+                gross_totals[org] = gross_earnings
+        
+    return gross_totals
+
+def option():
+    print("-----"*10)
+    print("Ola User!! Qual Opcao do nosso sistema voce gostaria de escolher? (Digite um numero)")
+    print("1 - Para abrir a Media de nota da Marvel vs DC")
+    print("2 - Para abrir quem tem o maior orçamentos nos filmes")
+    print("3 - Para visualizar quem tem o maior faturamento")
+    print("4 - Para visualizar a nota dos filmes")
+    print("0 - Para sair")
+    print("-----"*10)
+
+    while True:
+            Option = input()
+            if Option.isdigit():
+                Option = int(Option)
+                return Option
+            else:
+                print("Digite um numero para selecionar uma opcao")
+                return Option
+        
+def option_budget():
+    print("-----"*10)
+    print("Selecione a Opcao que deseja visualizar")
+    print("1 - Ver as Notas gerais")
+    print("2 - Ver a diferença das notas")
+    print("3 - Visualizar a media se a marvel nunca tivesse feito o melhor filme deles - Avengers")
+    print("0 - Para Voltar")
+
+    while True:
+            option_bud = input()
+            if option_bud.isdigit():
+                option_bud = int(option_bud)
+                return option_bud
+            else:
+                print("Digite um numero para selecionar uma opcao")
+                return option_bud
+            
+
+def option_gross():
+    print("-----"*10)
+    print("1 - Ver a empresa com o maior faturamento")
+    print("2 - Ver a media da empresa com o menor faturamento")
+    print("3 - A diferenca entre as duas empresas")
+    print("4 - Ver a diferenca se a Marvel nao tivesse feito Avengers")
+    print("0 - Para voltar")
+
+    while True:
+            Option_gross = input()
+            if Option_gross.isdigit():
+                Option_gross = int(Option_gross)
+                return Option_gross
+            else:
+                print("Digite um numero para selecionar uma opcao")
+                return Option_gross
+            
+def option_rate():
+    print("-----"*10)
+    print("1 - Para definir a nota:")
+    print("0 - Para sair")
+
+    while True:
+            Option_rate = input()
+            if Option_rate.isdigit():
+                Option_rate = int(Option_rate)
+                return Option_rate
+            else:
+                print("Digite um numero para selecionar uma opcao")
+                return Option_rate
