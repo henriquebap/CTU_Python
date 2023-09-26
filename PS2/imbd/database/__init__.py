f225aa6dd0e1981a9f1da23307e5fc6de618c166
Henriquebap <henriquebaptista2003@gmail.com>
2023-09-25 21:59:45 -0300

    Gitignore Once again

--- PS2/imbd/database/__init__.py
+++ PS2/imbd/database/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemyDatabaseUrl import password
+from PS2.imbd.sqlpw import password
 
 SQLALCHEMY_DATABASE_URL = password
 
